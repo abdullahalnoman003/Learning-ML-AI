aa8837fdb1a22983a0eccd600a705154463c084d
abdullahalnoman003 <noman22622@gmail.com>
2026-01-10 20:40:33 +0600

    Functions

---
+++ Python Foundation/functions.py
@@ -0,0 +1,13 @@
+# Functions are the codes that can be called several time in several needs
+# Functions can be with or without parameter
+def add(a, b):
+    print(a + b)
+
+add(10, 20)
+add(5, 7)
+
+
+def greet(name):
+    print("Hello", name)
+greet("Noman")
+greet("Abdullah")
